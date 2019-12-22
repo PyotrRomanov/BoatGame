cfe07417656a8a34035dbdee9e842d3d5e57e58e
DESKTOP-OG3SE7I\Bor <bor.greve@gmail.com>
2019-12-22 18:14:46 +0100

    Improved transition between camera behaviours.

--- BoatGame/Assets/Scripts/CameraScripts/StillDefaultCamera.cs
+++ BoatGame/Assets/Scripts/CameraScripts/StillDefaultCamera.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class StillDefaultCamera : CameraBehaviour
 {
+    float maxZoom = 0;
+    float xLerp = 1;
+
     public StillDefaultCamera() : base(0)
     {
 
@@ -11,9 +14,18 @@ public class StillDefaultCamera : CameraBehaviour
 
     public override void DoCameraBehaviour(Transform camera, BoatMovement boat)
     {
-        base.zoomoutValue = Mathf.Lerp(zoomoutValue, 1.5f * Mathf.Abs(boat.speed), 0.3f * Time.deltaTime);
+        if(boat.speed > 0.5f)
+        {
+            maxZoom = 3;
+        }
+
+        if(maxZoom == 3)
+        {
+            xLerp = Mathf.Lerp(xLerp, 0, 0.15f * Time.deltaTime);
+        }
+        base.zoomoutValue = Mathf.Lerp(zoomoutValue, maxZoom, 0.1f * Time.deltaTime);
         Camera.main.orthographicSize = 1f + zoomoutValue;
         float y = Mathf.Clamp(boat.transform.position.y, -4.9f + Camera.main.orthographicSize, 3000f);
-        camera.position = new Vector3(boat.transform.position.x, y, camera.position.z);
+        camera.position = new Vector3(boat.transform.position.x * xLerp + 0.3f * (1-xLerp), y, camera.position.z);
     }
 }
